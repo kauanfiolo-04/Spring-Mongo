faae45cb7a9921335530c68e66e0bdbb5930747e
kauanfiolo-04 <kauanfiolo@hotmail.com>
2026-09-10 17:14:03 -0300

    fix: typo

--- src/main/java/com/javacourse_fiolo04/spring_mongo/services/PostService.java
+++ src/main/java/com/javacourse_fiolo04/spring_mongo/services/PostService.java
@@ -22,6 +22,6 @@ public class PostService {
     }
 
     public List<Post> findByTitle(String text) {
-        return repository.findByTitleContaining(text);
+        return repository.findByTitleContainingIgnoreCase(text);
     }
 }
